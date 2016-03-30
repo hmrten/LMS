d4957600a1e8aa700f002ca691004ebdad43e0ab
Pernilla <pernilla.z@gmail.com>
2016-03-30 10:34:44 +0200

    HtmlHelper till actic class, styling i site.css

---
+++ LMS/HtmlHelpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LMS
+{
+	public static class HtmlHelpers
+	{
+		public static MvcHtmlString IsActive(this HtmlHelper htmlHelper, string path, string activeClass, string inActiveClass = "")
+		{
+			var routeData = htmlHelper.ViewContext.RouteData;
+
+			var routeController = routeData.Values["controller"].ToString();
+			var routeAction = routeData.Values["action"].ToString();
+			var routeSection = routeData.Values["section"].ToString();
+
+			var routePath = string.Format("{0}/{1}/{2}", routeController, routeAction, routeSection);
+			var returnActive = (path == routePath);
+
+			return new MvcHtmlString(returnActive ? activeClass : inActiveClass);
+		}
+
+	}
+}
