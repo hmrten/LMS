7bcff7d219dbf436daaa0dd762851bab716466ce
Pernilla <pernilla.z@gmail.com>
2016-03-31 13:35:43 +0200

    Fixat så att dataService används istället för http

--- LMS/App/Teacher/Subject/subjectApp.js
+++ LMS/App/Teacher/Subject/subjectApp.js
@@ -13,36 +13,18 @@
             })
             .when('/Edit/:id', {
                 templateUrl: LMS.rootPath + 'App/Teacher/Subject/Views/subjectEditView.html',
-                controller: "subjectCtrl"
+                controller: "editCtrl"
         });
-
-        //$locationProvider.html5Mode({
-        //    enabled: true,
-        //    requireBase: false
-        //});
     });
 
-    app.controller('subjectCtrl', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
-
+    app.controller('subjectCtrl', ['$scope', '$routeParams', 'dataService', function ($scope, $routeParams, dataService) {
         getAllSubjects();
 
         function getAllSubjects() {
-            $http.get(LMS.rootPath + "Subject/List").then(function (resp) {
-                $scope.subjects = resp.data;
-            }, function (resp) {
-                $scope.message = resp.status + resp.statusText;
-            });
-        };
-
-        getOneSubject();
-
-        function getOneSubject() {
-            var id = $routeParams["id"];
-     
-            $http.get(LMS.rootPath + "Subject/GetSubject/" + id).then(function(resp) {
-                $scope.subject = resp.data;
-            }, function (resp) {
-                $scope.message = resp.status + resp.statusText;
+            dataService.get("Subject/List", function (data) {
+                $scope.subjects = data;
+            }, function (resp) { // Om det blir fel
+                $scope.message = resp.statusText;
             });
         };
 
@@ -51,7 +33,21 @@
                 name: $scope.name,
                 description: $scope.description
             };
-            $http.post(LMS.rootPath + "Subject/Create", data).then(function (resp) {
+            function onResponse(resp) {
+                $scope.message = resp.statusText;
+            };
+            dataService.post("Subject/Create", data, onResponse, onResponse);
+        };
+
+    }]);
+    app.controller('editCtrl', ['$scope', '$routeParams', 'dataService', function ($scope, $routeParams, dataService) {
+        getOneSubject();
+
+        function getOneSubject() {
+            var id = $routeParams["id"];
+            dataService.get("Subject/GetSubject/" + id, function (data) {
+                $scope.subject = data;
+            }, function (resp) { // Om det blir fel
                 $scope.message = resp.statusText;
             });
         };
@@ -62,11 +58,10 @@
                 name: $scope.subject.name,
                 description: $scope.subject.description
             };
-            
-            $http.post(LMS.rootPath + "Subject/Update", data).then(function (resp) {
+            function onResponse(resp) {
                 $scope.message = resp.statusText;
-                console.log(resp);
-            });
+            };
+            dataService.post("Subject/Update", data, onResponse, onResponse);
         };
     }]);
 
