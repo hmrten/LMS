ca5a17caf183266646cc7a549cd75f39a15569b0
Mårten Hansson <hmrten@gmail.com>
2016-04-01 10:49:46 +0200

    add role_id and role_name to user list and group by role id
    now returns a nested array of dicts keyd by role name and contains a
    list of users

--- LMS/Controllers/UserController.cs
+++ LMS/Controllers/UserController.cs
@@ -12,55 +12,62 @@ namespace LMS.Controllers
 {
     public class UserController : Controller
     {
-		private LMSContext db = new LMSContext();
-		private AppUserManager userManager;
+        private LMSContext db = new LMSContext();
+        private AppUserManager userManager;
 
-		public UserController()
-		{
-			userManager = AppUserManager.Create(new IntUserStore(db));
-		}
+        public UserController()
+        {
+            userManager = AppUserManager.Create(new IntUserStore(db));
+        }
 
-		public JsonResult GetRoles()
-		{
-			var q = db.Roles.Select(r => 
-				new
-				{
-					id = r.Id,
-					name = r.Name
-				});
+        public JsonResult GetRoles()
+        {
+            var q = db.Roles.Select(r =>
+                new
+                {
+                    id = r.Id,
+                    name = r.Name
+                });
 
-			return Json(q.ToList(), JsonRequestBehavior.AllowGet);
-		}
+            return Json(q.ToList(), JsonRequestBehavior.AllowGet);
+        }
 
-		public JsonResult GetUser(int id)
-		{
-			var role = userManager.GetRoles(id).First();
-			var user = db.Users.Where(s => s.Id == id).Select(s =>
-				new
-				{
-					id = s.Id,
-					fname = s.FirstName,
-					lname = s.LastName,
-					email = s.Email,
-					phone = s.PhoneNumber,
-					role = role
-				}).SingleOrDefault();
+        public JsonResult GetUser(int id)
+        {
+            var role = userManager.GetRoles(id).First();
+            var user = db.Users.Where(s => s.Id == id).Select(s =>
+                new
+                {
+                    id = s.Id,
+                    fname = s.FirstName,
+                    lname = s.LastName,
+                    email = s.Email,
+                    phone = s.PhoneNumber,
+                    role_id = s.Roles.FirstOrDefault().RoleId,
+                    role_name = role
+                }).SingleOrDefault();
 
-			return Json(user, JsonRequestBehavior.AllowGet);
-		}
+            return Json(user, JsonRequestBehavior.AllowGet);
+        }
 
-		public JsonResult List()
-		{
-			var q = db.Users.Select(s =>
-				new
-				{
-					id = s.Id,
-					fname = s.FirstName,
-					lname = s.LastName
-				});
-
-			return Json(q.ToList(), JsonRequestBehavior.AllowGet);
-		}
+        public JsonResult List()
+        {
+            var q = (from u in db.Users
+                     join r in db.Roles on u.Roles.FirstOrDefault().RoleId equals r.Id
+                     select new
+                     {
+                         id = u.Id,
+                         fname = u.FirstName,
+                         lname = u.LastName,
+                         role_id = r.Id,
+                         role_name = r.Name
+                     }).GroupBy(g => g.role_name).Select(x => new
+                     {
+                         role = x.Key,
+                         users = x.ToList()
+                     });
+            return Json(q, JsonRequestBehavior.AllowGet);
+        }
 
     }
 }
